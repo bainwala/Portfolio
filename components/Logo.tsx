ef0f1598e326079f4071d5dc326228eaa5961560
Himanshu Bainwala <himanshubainwala@Himanshus-MBP.domain>
2023-10-22 01:37:46 -0500

    Minor Changes

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
+import { AspectRatio } from './ui/aspect-ratio'
 
 function Logo() {
   return (
-    <Link href={'/'}>Logo</Link>
+    <Link href={'/'} prefetch={false} className='overflow-hidden'>
+        <div className='flex items-center w-72 h-14'>
+            <AspectRatio
+            ratio={16/9}
+            className='flex items-center justify-center'>
+                Logo
+            </AspectRatio>
+        </div>
+    </Link>
   )
 }
 
